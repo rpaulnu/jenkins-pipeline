252091954021dc3caf0a4982a76d378b71d834d6
Rodrigo Nuñez Paul <rnunepau@everis.com>
2021-02-04 17:58:50 +0100

    hide credentials

--- script-pipe.groovy
+++ script-pipe.groovy
@@ -5,14 +5,16 @@ pipeline{
 	agent any 
 		stages{
 			stage('Login'){
+
 				steps{
-					script{
-						def loginRequest =   "curl -H 'Content-Type: application/x-www-form-urlencoded' -X POST -d username=techuser -d password=Avantmoney1 https://eu1.anypoint.mulesoft.com/accounts/login".execute().text
+						def loginRequest =   "curl -H 'Content-Type: application/x-www-form-urlencoded' -X POST -d username=user -d password=password https://eu1.anypoint.mulesoft.com/accounts/login".execute().text
 						def slurper = new JsonSlurper().parseText(loginRequest)
 						def access_token = slurper.access_token
+					script{
+						
 						println 'LOGIN IN ANYPOINT PLATFORM'
 		
-						println '${access_token}'
+						println ${access_token}
 
        					
 
